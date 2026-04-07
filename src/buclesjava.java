5d19aafa398050d395bc927134233401003169df
vnangz <igutierrezcontreras@danielcastelao.org>
2026-04-07 12:02:15 +0200

    inicio

---
+++ src/buclesjava.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class buclesjava {
+    public static void main (String[] args){
+
+    }
+    Scanner teclado = new Scanner(System.in);
+    int opcion = 0;
+
+    while (opcion != 3){
+        System.out.println("Elige una opcion");
+        System.out.println("1. Saludar");
+        System.out.println("2. Despedirse");
+        System.out.println("3. Salir");
+        String op = teclado.nextLine();
+    }
+}
