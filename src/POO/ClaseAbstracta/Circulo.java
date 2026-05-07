40c4e7902efa6cad593dcf3e15c772821fce4501
ivgrz <igutierrezcontreras@danielcastelao.org>
2026-05-07 13:49:06 +0200

    Clases_Abstractas

---
+++ src/POO/ClaseAbstracta/Circulo.java
@@ -0,0 +1,38 @@
+package POO.ClaseAbstracta;
+/*
+Clase que implementa la clase abstracta Punto
+Para que no falle implementar un metodo abstracto de la clase padre
+O convertir esta clase en abstracta (pero no te deja instanciar objetos)
+ */
+public class Circulo extends Punto{
+
+    private double radio;
+    public Circulo(int x, int y, double radio){
+        super(x,y);
+        this.radio = radio;
+
+    }
+    public double getRadio() {
+
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    @Override
+    public double calcularArea() {
+        return Math.PI * Math.pow(radio,2);
+    }
+    @Override
+    public double calcularPerimetro(){
+        return 2* Math.PI * radio;
+    }
+
+    public String toString(){
+        return super.toString() + ", radio = " + radio;
+    }
+
+
+}
---
+++ src/POO/ClaseAbstracta/Punto.java
@@ -0,0 +1,41 @@
+package POO.ClaseAbstracta;
+
+public abstract class Punto {
+
+    private int x;
+    private int y;
+
+    public Punto(int x, int y){
+        this.x = x;
+        this.y = y;
+
+
+    }
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    public abstract double calcularArea();
+
+    public abstract double calcularPerimetro();
+    @Override
+    public String toString(){
+        return "Coordenadas del punto: " +"( " + getX() + " , " + getY() + " )";
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
