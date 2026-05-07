135d68d7904ef17d95dacca1d68da7489c4ec6e1
ivgrz <igutierrezcontreras@danielcastelao.org>
2026-05-07 14:39:40 +0200

    Definicion_ejemplos_interfaces_2

--- src/POO/interfaces/Animal.java
+++ src/POO/interfaces/Animal.java
@@ -5,15 +5,36 @@ package POO.interfaces;
 interface Son{
     //La interfaz es el "Manual de Instrucciones" que da habilidades (acciones obligatorias).
     void voz();
+    default void dormir(){
+        System.out.println("zzz...");
+    }
+    static void bostezo(){
+        System.out.println("*Bosteza*");
+
+    }
+}
+interface viviparo{
+    boolean viviparo = true;
 }
 
 
 public class Animal {
     int numpatas;
+    String tespecie;
+    String tnombre;
 
-    public Animal(int patas){
+    public Animal(String especie, String nombre, int patas){
         numpatas = patas;
+        tespecie = especie;
+        tnombre = nombre;
 
+    }
+    public static void RascarConPata(){
+        System.out.println("(Rascando...)");
+    }
+
+    public String toString(){
+        return "Especie: " + this.tespecie + "\n" + "Nombre: " + this.tnombre + "\n" + "Cantidad de patas: " + numpatas;
     }
 }
 
--- src/POO/interfaces/Can.java
+++ src/POO/interfaces/Can.java
@@ -1,14 +1,26 @@
 package POO.interfaces;
 
 
-public class Can extends Animal implements Son{
+public class Can extends Animal implements Son, viviparo{
     // La clase concreta es el "Resultado Final" que une todo y se puede usar en el main.
-        public Can(int patas){
-            super(patas);
+        public Can(String especie, String nombre, int patas){
+            super(especie, nombre, patas);
 
         }
 
         public void voz(){
-            System.out.println("Guau");
+            System.out.println("Guau guau");
         }
+        public String toString(){
+            return super.toString();
+        }
+
+    public static void main(String[] args) {
+        Can c = new Can("Perro","Max",4);
+        System.out.println(Can.viviparo);
+        c.dormir();
+        Animal.RascarConPata();
+        Son.bostezo();
+        System.out.println(c);
+    }
 }
